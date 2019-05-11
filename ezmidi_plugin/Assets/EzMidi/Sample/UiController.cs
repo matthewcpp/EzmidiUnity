7e96ba0ebf40b29fea71812924b9c73bc465da83
Matthew LaRocca <mlarocca.cpp@gmail.com>
2019-05-11 11:23:19 -0700

    Get Note events on windows

---
+++ ezmidi_plugin/Assets/EzMidi/Sample/UiController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour {
+
+    public EzMidi.Connection EzMidi;
+    public Dropdown MidiSourcesDropdown;
+
+	void Start ()
+    {
+        var dropdownOptions = new List<Dropdown.OptionData>();
+        var midiSources = EzMidi.GetSourceNames();
+
+        foreach (var midiSource in midiSources)
+            dropdownOptions.Add(new Dropdown.OptionData(midiSource));
+
+        MidiSourcesDropdown.options = dropdownOptions;
+    }
+	
+    public void ConnectSelectedSource()
+    {
+        EzMidi.NoteOn += DebugNoteOn;
+        EzMidi.NoteOff += DebugNoteOff;
+
+        Debug.Log(string.Format("Connect source: {0}", MidiSourcesDropdown.value));
+        EzMidi.ConnectSource(MidiSourcesDropdown.value);
+    }
+
+    void DebugNoteOn(int note, int velocity)
+    {
+        Debug.Log(string.Format("Note On: {0}, velocity: {1}", note, velocity));
+    }
+
+    void DebugNoteOff(int note, int velocity)
+    {
+        Debug.Log(string.Format("Note Off: {0}, velocity: {1}", note, velocity));
+    }
+}
---
+++ ezmidi_plugin/Assets/EzMidi/Scripts/Connection.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace EzMidi
+{
+    public class Connection : MonoBehaviour
+    {
+        private IntPtr ezmidiContext = IntPtr.Zero;
+        private bool isInit;
+
+        public delegate void NoteEvent(int note, int velocity);
+        public delegate void LogEvent(string message);
+
+        public NoteEvent NoteOn;
+        public NoteEvent NoteOff;
+        public LogEvent Log;
+
+        private Native.Ezmidi_Event midiEvent = new Native.Ezmidi_Event();
+
+        void Awake()
+        {
+            InitNativeLibrary();
+        }
+
+        void Update()
+        {
+            if (!isInit) return;
+
+            while (Native.ezmidi_pump_events(ezmidiContext, ref midiEvent) != 0)
+            {
+                switch (midiEvent.type)
+                {
+                    case Native.Ezmidi_EventType.EZMIDI_NOTE:
+                        ProcessNoteEvent(midiEvent);
+                        break;
+                }
+            }
+        }
+
+        private void ProcessNoteEvent(Native.Ezmidi_Event ezmidiEvent)
+        {
+            switch (ezmidiEvent.note_event.detail)
+            {
+                case Native.Ezmidi_NoteEventId.EZMIDI_NOTEEVENT_ON:
+                    NoteOn(ezmidiEvent.note_event.note, ezmidiEvent.note_event.velocity);
+                    break;
+
+                case Native.Ezmidi_NoteEventId.EZMIDI_NOTEEVENT_OFF:
+                    NoteOff(ezmidiEvent.note_event.note, ezmidiEvent.note_event.velocity);
+                    break;
+            }
+        }
+
+        private static void LogMessage(string message, IntPtr user_data)
+        {
+            Debug.Log(message);
+        }
+
+        private void InitNativeLibrary()
+        {
+            var config = new Native.Ezmidi_Config();
+            Native.ezmidi_config_init(ref config);
+            config.log_func = LogMessage;
+
+            ezmidiContext = Native.ezmidi_create(ref config);
+
+            if (ezmidiContext == IntPtr.Zero)
+                throw new EzMidi.Exception("Ezmidi Initialization failed.");
+
+            Debug.Log("Created ezmidi Context!");
+            isInit = true;
+
+            Native.ezmidi_connect_source(ezmidiContext, 0);
+        }
+
+        public List<String> GetSourceNames()
+        {
+            List<string> sourceNames = new List<string>();
+
+            if (isInit)
+            {
+                int numSources = Native.ezmidi_get_source_count(ezmidiContext);
+                for (int i = 0; i < numSources; i++)
+                {
+                    IntPtr strPtr = Native.ezmidi_get_source_name(ezmidiContext, i);
+                    string name = Marshal.PtrToStringAnsi(strPtr);
+                    sourceNames.Add(name);
+                }
+            }
+
+            return sourceNames;
+        }
+
+        public void ConnectSource(int index)
+        {
+            Native.ezmidi_connect_source(ezmidiContext, index);
+        }
+
+        private void OnDestroy()
+        {
+            if (ezmidiContext != IntPtr.Zero)
+            {
+                isInit = false;
+                Native.ezmidi_destroy(ezmidiContext);
+                ezmidiContext = IntPtr.Zero;
+
+                Debug.Log("Destroyed ezmidi Context!");
+            }
+        }
+    }
+
+    public class Exception : System.Exception
+    {
+        public Exception(string message)
+                : base(message)
+        {
+        }
+    }
+}
+
