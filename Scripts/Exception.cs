37ee1585838f297b6469cd04ce15a64f8aa8a09d
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-04-29 23:34:27 -0700

    Move Exception Class to its own file

---
+++ Scripts/Exception.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EzMidi
+{
+    public class Exception : System.Exception
+    {
+        public Exception(string message)
+                : base(message)
+        {
+        }
+    }
+}
